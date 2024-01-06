3d3abe140c88091f9666541411bdd8f0a5a48a6c
Audrey Serene <audrey.serene@gmail.com>
2024-01-06 14:32:19 -0600

    Add save system and settings

---
+++ apps/save/save_resource.gd
@@ -0,0 +1,3 @@
+class_name SaveResource extends Resource
+
+@export var settings := SettingsResource.new()
---
+++ apps/settings/settings_resource.gd
@@ -0,0 +1,5 @@
+class_name SettingsResource extends Resource
+
+
+@export_category("Movement")
+@export var tap_to_move: bool = true
