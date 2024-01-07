9e1903858750ec92db58b6037fdcf46156eced15
Audrey Serene <audrey.serene@gmail.com>
2024-01-07 17:43:16 -0600

    Expose inventory system to Lua API (resolves #53)

--- apps/inventory/classes/inventory_resource.gd
+++ apps/inventory/classes/inventory_resource.gd
@@ -5,3 +5,7 @@ class_name InventoryResource extends ModdableResource
 	set(value):
 		item_instances = value
 		emit_changed()
+
+
+func lua_fields() -> Array[String]:
+	return super() + ["item_instances"]
