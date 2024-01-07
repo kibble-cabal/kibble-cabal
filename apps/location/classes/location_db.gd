2c5f8d4ab5c9d131405e091080ac9ceed31ba940
Audrey Serene <audrey.serene@gmail.com>
2024-01-07 17:03:42 -0600

    Add `LocationDB` class (resolves #48)

---
+++ apps/location/classes/location_db.gd
@@ -0,0 +1,24 @@
+# LocationDB
+extends Node
+
+signal location_registered(location: LocationResource)
+signal location_unregistered(location: LocationResource)
+
+
+var registered_locations: Array[LocationResource] = []
+
+
+func register(location: LocationResource) -> void:
+	registered_locations.append(location)
+	location_registered.emit(location)
+
+
+func unregister(location: LocationResource) -> void:
+	registered_locations.erase(location)
+	location_unregistered.emit(location)
+
+
+func find(location_name: String) -> LocationResource:
+	for location in registered_locations:
+		if location.name == location_name: return location
+	return null
