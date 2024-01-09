bed403273ddb51b7b37e75b34a19b3003e6df263
Audrey Serene <audrey.serene@gmail.com>
2024-01-08 18:54:34 -0600

    Add `SettingDefinitionResource` class (resolves #70)

---
+++ apps/settings/classes/setting_definition_resource.gd
@@ -0,0 +1,6 @@
+class_name SettingDefinitionResource extends ModdableResource
+
+@export var id: String
+@export var display_name: String
+@export var display_description: String
+@export var ui: PackedScene
