bfa7b9ef34d08ae9d99614e7de50163e34d2eee0
Audrey Serene <audrey.serene@gmail.com>
2024-01-08 20:08:26 -0600

    Add expansion pack discover (resolves #79)

--- apps/expansion_pack/classes/expansion_pack_db.gd
+++ apps/expansion_pack/classes/expansion_pack_db.gd
@@ -9,7 +9,12 @@ signal pack_unregistered(pack: ExpansionPackResource)
 var registered_packs: Array[ExpansionPackResource] = []
 
 
+func _ready() -> void:
+	discover()
+
+
 func register(pack: ExpansionPackResource) -> void:
+	print("Registering expansion pack: ", pack.display_name)
 	registered_packs.append(pack)
 	pack_registered.emit(pack)
 
@@ -23,6 +28,16 @@ func find(pack_name: String) -> ExpansionPackResource:
 	for pack in registered_packs:
 		if pack.name == pack_name: return pack
 	return null
+
+
+func discover(entry_dir: String = "res://") -> void:
+	for dir in DirAccess.get_directories_at(entry_dir):
+		discover(entry_dir.path_join(dir))
+	for file in DirAccess.get_files_at(entry_dir):
+		if file.ends_with("expansionpack.tres"):
+			var resource := ResourceLoader.load(entry_dir.path_join(file))
+			if resource is ExpansionPackResource:
+				register(resource)
 
 
 func lua_fields() -> Array[String]:
