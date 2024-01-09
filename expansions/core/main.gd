299d46cf30ee36fb634b9fa562e95a86d2da99bf
Audrey Serene <audrey.serene@gmail.com>
2024-01-09 17:10:38 -0600

    Add placeholder items to core expansion

--- expansions/core/main.gd
+++ expansions/core/main.gd
@@ -9,7 +9,13 @@ const Settings = [
 	preload("res://expansions/core/settings/assets/resources/tap_to_move.tres"),
 ]
 
+const Items = [
+	preload("res://expansions/core/item/assets/resources/flower.tres"),
+	preload("res://expansions/core/item/assets/resources/food_bowl.tres"),
+]
+
 
 func _init() -> void:
 	Locations.map(LocationDB.register)
 	Settings.map(SettingDefinitionDB.register)
+	Items.map(ItemDB.register)
