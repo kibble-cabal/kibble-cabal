9be9675da343f4d062bdad64b3e3f35e5ffa6aec
Audrey Serene <audrey.serene@gmail.com>
2024-01-08 19:57:40 -0600

    Add `ExpansionPackDB` class (resolves #76)

---
+++ apps/expansion_pack/classes/expansion_pack_db.gd
@@ -0,0 +1,29 @@
+# ExpansionPackDB
+extends Node
+
+
+signal pack_registered(pack: ExpansionPackResource)
+signal pack_unregistered(pack: ExpansionPackResource)
+
+
+var registered_packs: Array[ExpansionPackResource] = []
+
+
+func register(pack: ExpansionPackResource) -> void:
+	registered_packs.append(pack)
+	pack_registered.emit(pack)
+
+
+func unregister(pack: ExpansionPackResource) -> void:
+	registered_packs.erase(pack)
+	pack_unregistered.emit(pack)
+
+
+func find(pack_name: String) -> ExpansionPackResource:
+	for pack in registered_packs:
+		if pack.name == pack_name: return pack
+	return null
+
+
+func lua_fields() -> Array[String]:
+	return ["registered_packs", "find"]
