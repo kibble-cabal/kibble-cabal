7ae2523edbb2c7afe0d6110531e883bcc88f0dc1
Audrey Serene <audrey.serene@gmail.com>
2024-01-08 18:58:50 -0600

    Add `SettingDefinitionDB` class (resolves #71)

---
+++ apps/settings/classes/setting_definition_db.gd
@@ -0,0 +1,30 @@
+# SettingDefinitionDB
+extends Node
+
+signal setting_registered(setting: SettingDefinitionResource)
+signal setting_unregistered(setting: SettingDefinitionResource)
+
+
+var registered_settings: Array[SettingDefinitionResource] = []
+
+
+func register(setting: SettingDefinitionResource) -> void:
+	registered_settings.append(setting)
+	setting_registered.emit(setting)
+
+
+func unregister(setting: SettingDefinitionResource) -> void:
+	registered_settings.erase(setting)
+	setting_unregistered.emit(setting)
+
+
+func find_by_name(setting_name: String) -> SettingDefinitionResource:
+	for setting in registered_settings:
+		if setting.display_name == setting_name: return setting
+	return null
+
+
+func find_by_id(setting_id: String) -> SettingDefinitionResource:
+	for setting in registered_settings:
+		if setting.id == setting_id: return setting
+	return null
