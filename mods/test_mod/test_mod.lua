57b170978e91a9fc0c990551b4c25c4d4554e72b
Audrey Serene <audrey.serene@gmail.com>
2024-01-12 19:03:41 -0600

    Update test mod

--- mods/test_mod/test_mod.lua
+++ mods/test_mod/test_mod.lua
@@ -1,8 +1,11 @@
-Log.start_section("TestMod", "Running test mod...")
-local current_location = GetLocationSystem().current_location
-if current_location ~= nil then
-    Log.from("TestMod", "Detected current location: " .. current_location.name)
-else
-    Log.warning_from("TestMod", "Warning: Island location not found!")
+function OnLocationEntered(location)
+    Log.start_section("TestMod", "Running test mod...")
+    Log.bullet("Detected location entrance!")
+    local current_location = GetLocationSystem().current_location
+    if current_location ~= nil then
+        Log.bullet("Current location is: " .. current_location.name)
+    else
+        Log.warning("Warning: Island location not found!")
+    end
+    Log.end_section("TestMod")
 end
-Log.end_section("TestMod")
