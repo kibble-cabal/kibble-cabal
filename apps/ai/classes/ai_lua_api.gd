2bcac849dfc90dde7b712b6ec91d6e6fd8e47591
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 01:13:51 -0600

    Expose AI signals to Lua API (#155)

--- apps/ai/classes/ai_lua_api.gd
+++ apps/ai/classes/ai_lua_api.gd
@@ -3,3 +3,14 @@ class_name AILuaAPI extends ExposeLuaAPI
 
 func expose_variables(lua: LuaAPI) -> void:
 	lua.push_variant("GetSubtreeDB", func(): return SubtreeDB)
+
+
+func expose_hooks(lua: LuaAPI) -> void:
+	SubtreeDB.subtree_registered.connect(
+		func(subtree: SubtreeResource) -> void:
+			lua.call_function("OnSubtreeRegistered", [subtree])
+	)
+	SubtreeDB.subtree_unregistered.connect(
+		func(subtree: SubtreeResource) -> void:
+			lua.call_function("OnSubtreeUnregistered", [subtree])
+	)
