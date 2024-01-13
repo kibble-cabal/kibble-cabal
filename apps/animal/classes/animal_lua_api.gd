a6a3e977ac9063680fb85efb7ae22cf134ac8e9b
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 01:15:13 -0600

    Expose animal signals to Lua API (#155)

--- apps/animal/classes/animal_lua_api.gd
+++ apps/animal/classes/animal_lua_api.gd
@@ -3,3 +3,14 @@ class_name AnimalLuaAPI extends ExposeLuaAPI
 
 func expose_variables(lua: LuaAPI) -> void:
 	lua.push_variant("GetAnimalDB", func(): return AnimalDB)
+
+
+func expose_hooks(lua: LuaAPI) -> void:
+	AnimalDB.animal_registered.connect(
+		func(animal: SubtreeResource) -> void:
+			lua.call_function("OnAnimalRegistered", [animal])
+	)
+	AnimalDB.animal_unregistered.connect(
+		func(animal: AnimalResource) -> void:
+			lua.call_function("OnAnimalUnregistered", [animal])
+	)
