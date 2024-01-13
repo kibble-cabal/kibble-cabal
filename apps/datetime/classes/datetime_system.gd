35485e0c553ee05a65e9443c4f32c366a354f4c7
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 01:21:35 -0600

    Remove `DatetimeSystem`'s pause implementation, connect time ticking to game mode instead

--- apps/datetime/classes/datetime_system.gd
+++ apps/datetime/classes/datetime_system.gd
@@ -2,8 +2,6 @@
 extends Node
 
 signal ticked
-signal unpaused
-signal paused
 
 var timer := Timer.new()
 
@@ -12,13 +10,6 @@ var resource: DatetimeResource:
 		if SaveSystem.current_save and SaveSystem.current_save.datetime:
 			return SaveSystem.current_save.datetime
 		return null
-
-var is_paused: bool = false:
-	set(value):
-		match [is_paused, value]:
-			[true, false]: unpaused.emit()
-			[false, true]: paused.emit()
-		is_paused = value
 
 
 func _ready() -> void:
@@ -29,6 +20,8 @@ func _ready() -> void:
 	
 	SaveSystem.save_opened.connect(_on_save_changed)
 	SaveSystem.save_closed.connect(_on_save_changed)
+	GameModeSystem.game_mode_entered.connect(_on_game_mode_entered)
+	GameModeSystem.game_mode_exited.connect(_on_game_mode_exited)
 
 
 ## How long the timer should wait before incrementing the current time ([member DatetimeResource.current_time])
@@ -36,16 +29,8 @@ func get_wait_time() -> float:
 	return DatetimeResource.TIME_SPEED * (resource.time_speed_multiplier if resource else 1.0)
 
 
-func pause() -> void:
-	is_paused = true
-
-
-func unpause() -> void:
-	is_paused = false
-
-
 func lua_fields() -> Array:
-	return ["get_wait_time", "is_paused", "pause", "unpause"]
+	return ["get_wait_time"]
 
 
 func _on_timeout() -> void:
@@ -56,3 +41,12 @@ func _on_timeout() -> void:
 
 func _on_save_changed(_save: SaveResource) -> void:
 	timer.wait_time = get_wait_time()
+
+
+func _on_game_mode_entered(mode: GameModeResource) -> void:
+	if mode != null: timer.paused = mode.world_paused
+	else: timer.paused = true
+
+
+func _on_game_mode_exited(_mode: GameModeResource) -> void:
+	timer.paused = true
