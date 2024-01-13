a56a93120f4cae3b9d4b2c75ba693fbd228c976a
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 11:25:00 -0600

    Expose sound effect signals to Lua API (#155)

--- apps/sound_effect/classes/sound_effect_lua_api.gd
+++ apps/sound_effect/classes/sound_effect_lua_api.gd
@@ -5,3 +5,14 @@ func expose_variables(lua: LuaAPI) -> void:
 	lua.push_variant("GetSoundEffectDB", func(): return SoundEffectDB)
 	lua.push_variant("GetSoundManager", func(): return SoundManager)
 	lua.push_variant("SoundHelper", Sound.new())
+
+
+func expose_hooks(lua: LuaAPI) -> void:
+	SoundEffectDB.sound_effect_registered.connect(
+		func(sound: AudioStream) -> void:
+			lua.call_function("OnSoundEffectRegistered", [sound])
+	)
+	SoundEffectDB.sound_effect_unregistered.connect(
+		func(sound: AudioStream) -> void:
+			lua.call_function("OnSoundEffectUnregistered", [sound])
+	)
