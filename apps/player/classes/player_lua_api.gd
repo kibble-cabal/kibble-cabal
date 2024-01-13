320523f5b27d5a1735d96ef0795f3201cc9138f3
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 10:55:04 -0600

    Add `player_spawned`, `player_despawned`, `before_player_spawned`, `before_player_despawned` signals, expose to Lua API (#155)

--- apps/player/classes/player_lua_api.gd
+++ apps/player/classes/player_lua_api.gd
@@ -3,3 +3,22 @@ class_name PlayerLuaAPI extends ExposeLuaAPI
 
 func expose_variables(lua: LuaAPI) -> void:
 	lua.push_variant("GetPlayerSystem", func(): return PlayerSystem)
+
+
+func expose_hooks(lua: LuaAPI) -> void:
+	PlayerSystem.player_spawned.connect(
+		func(node: Node) -> void:
+			lua.call_function("OnPlayerSpawned", [node])
+	)
+	PlayerSystem.player_despawned.connect(
+		func() -> void:
+			lua.call_function("OnPlayerDespawned", [])
+	)
+	PlayerSystem.before_player_spawned.connect(
+		func() -> void:
+			lua.call_function("OnBeforePlayerSpawned", [])
+	)
+	PlayerSystem.before_player_despawned.connect(
+		func(node: Node) -> void:
+			lua.call_function("OnBeforePlayerDespawned", [node])
+	)
--- apps/player/classes/player_system.gd
+++ apps/player/classes/player_system.gd
@@ -1,6 +1,11 @@
 # PlayerSystem
 extends Node
 
+signal player_spawned(node: PlayerBody2D)
+signal player_despawned()
+signal before_player_spawned()
+signal before_player_despawned(node: PlayerBody2D)
+
 const PlayerScene := preload("res://apps/player/scenes/player_scene.tscn")
 
 var player: PlayerResource:
@@ -19,6 +24,7 @@ func _ready() -> void:
 
 func spawn(location: LocationResource) -> void:
 	if player:
+		before_player_spawned.emit()
 		player_node = PlayerScene.instantiate()
 		var world_root := get_tree().get_first_node_in_group("world_root")
 		if world_root: 
@@ -27,12 +33,15 @@ func spawn(location: LocationResource) -> void:
 			player.current_position = player_node.position
 			player.current_location = location.name
 			Log.from(self, "Player spawned!")
+			player_spawned.emit(player_node)
 
 
 func despawn() -> void:
 	if player_node:
+		before_player_despawned.emit(player_node)
 		player_node.queue_free()
 		player_node = null
+		player_despawned.emit()
 
 
 func get_spawn_position(location: LocationResource) -> Vector2:
