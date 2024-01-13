4cb56b86abc2d098fb3c5fe2e955ca621db2d988
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 01:22:53 -0600

    Expose datetime system signals to Lua API (#155)

--- apps/datetime/classes/datetime_lua_api.gd
+++ apps/datetime/classes/datetime_lua_api.gd
@@ -4,3 +4,10 @@ class_name DatetimeLuaAPI extends ExposeLuaAPI
 func expose_variables(lua: LuaAPI) -> void:
 	lua.push_variant("GetDatetimeSystem", func(): return DatetimeSystem)
 	lua.push_variant("DatetimeHelper", DatetimeHelper.new())
+
+
+func expose_hooks(lua: LuaAPI) -> void:
+	DatetimeSystem.ticked.connect(
+		func() -> void:
+			lua.call_function("OnDatetimeTicked", [])
+	)
