470ed4d5a130a67c226f8e91903b4c3d3ee207ba
Audrey Serene <audrey.serene@gmail.com>
2024-01-13 12:18:14 -0600

    Add `MusicDB` class (resolves #122)

---
+++ apps/music/classes/music_db.gd
@@ -0,0 +1,28 @@
+# MusicDB
+extends Node
+
+signal music_registered(music: MusicResource)
+signal music_unregistered(music: MusicResource)
+
+
+var registered_music: Array[MusicResource] = []
+
+
+func register(music: MusicResource) -> void:
+	registered_music.append(music)
+	music_registered.emit(music)
+
+
+func unregister(music: MusicResource) -> void:
+	registered_music.erase(music)
+	music_unregistered.emit(music)
+
+
+func find(music_name: String) -> MusicResource:
+	for music in registered_music:
+		if music.name == music_name: return music
+	return null
+
+
+func lua_fields() -> Array:
+	return ["register", "unregister", "find", "registered_music"]
