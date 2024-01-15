f85ba97819b04ec91096361d7c9ba9efd6eb29e9
Audrey Serene <audrey.serene@gmail.com>
2024-01-15 10:33:34 -0600

    Start adding need fulfillment behavior tree (#183)

--- main.gd
+++ main.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var live_mode := GameModeDB.find("Live")
 @onready var live_paused_mode := GameModeDB.find("Live/Paused")
 
+@onready var flower := ItemDB.find_by_id("core__flower")
+
 
 func _ready() -> void:
 	SaveSystem.save_opened.connect(_on_save_opened)
@@ -15,4 +17,12 @@ func _on_save_opened(_save: SaveResource) -> void:
 	await get_tree().process_frame
 	LocationSystem.enter(island)
 	GameModeSystem.to(live_mode)
+
+
+func _debug_remake_location_inventory() -> void:
+	var state := SaveSystem.current_save.get_or_create_location_state("Island")
+	var item := flower.instantiate()
+	item.location = Vector2(100, 100)
+	state.inventory = InventoryResource.new()
+	state.inventory.item_instances.append(item)
 	
