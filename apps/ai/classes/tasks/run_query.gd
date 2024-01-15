9d3fab21988a027bdc75aebf03891b72ffaf7633
Audrey Serene <audrey.serene@gmail.com>
2024-01-15 12:34:29 -0600

    Convert `node_path` param to `BBNode` param in `BTRunQuery` class, clean up code
    This is the more idiomatic way to do it.

--- apps/ai/classes/tasks/run_query.gd
+++ apps/ai/classes/tasks/run_query.gd
@@ -1,26 +1,34 @@
 @tool
 class_name BTRunQuery extends BTAction
 
-@export var node_path: NodePath
+@export var node: BBNode
 @export var query: Query = null
-@export var result_var: StringName
+@export var result_var: String
 
 
 func _generate_name() -> String:
+	var query_name := (
+		query.resource_name if not query.resource_name.is_empty() 
+		else query.resource_path if query 
+		else "???"
+	)
 	var result_string := ", set Blackboard.{0} to result".format([result_var]) if result_var else ""
-	if len(query.resource_name):
-		return "Run Query \"{0}\"".format([query.resource_name]) + result_string
-	if len(query.resource_path): 
-		return "Run Query \"{0}\"".format([query.resource_path]) + result_string
-	return "Run Query" + result_string
+	return "Run Query \"{0}\"{1}".format([query_name, result_string])
+
+
+func _get_configuration_warning() -> PackedStringArray:
+	var warning := PackedStringArray()
+	if not node: warning.append("Node is missing!")
+	if not query: warning.append("Query is missing!")
+	return warning
 
 
 func _tick(_delta: float) -> Status:
-	var node := agent.get_node(node_path)
-	if not node:
+	if not query: return FAILURE
+	var node_value = node.get_value(agent, blackboard)
+	if not node_value or not node_value is Node:
 		return FAILURE
-	var result = query.query(node)
-	if not result_var:
-		return FAILURE
-	blackboard.set_var(result_var, result)
+	var result = query.query(node_value)
+	if result_var:
+		blackboard.set_var(result_var, result)
 	return SUCCESS
