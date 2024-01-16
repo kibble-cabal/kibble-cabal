334797e4611927cd4fd9995911d687a1db68dbbb
Audrey Serene <audrey.serene@gmail.com>
2024-01-16 15:43:03 -0600

    Combine `navigate_to_blackboard_position` and `navigate_to_position` AI actions

--- apps/ai/classes/tasks/navigation/navigate_to_position.gd
+++ apps/ai/classes/tasks/navigation/navigate_to_position.gd
@@ -4,7 +4,7 @@ class_name BTNavigateToPosition extends BTNavigate
 ## Sets the target position for the given [NavigationAgent].
 ## If the navigation stops but the target is not reached, this task fails.
 
-@export var target_position: Vector2
+@export var target_position: BBVector2
 
 
 func _generate_name() -> String:
@@ -12,4 +12,4 @@ func _generate_name() -> String:
 
 
 func get_navigation_position() -> Vector2:
-	return target_position
+	return target_position.get_value(agent, blackboard)
