3448482c8af71d986d6be4cd709ecb30eb4647b8
Audrey Serene <audrey.serene@gmail.com>
2024-01-24 13:22:34 -0600

    Add `detection_radius` property to `AnimalResource` class (resolves #206)

--- apps/animal/classes/animal_resource.gd
+++ apps/animal/classes/animal_resource.gd
@@ -4,7 +4,14 @@ class_name AnimalResource extends ModdableResource
 @export_range(0, 1) var speed: float = 0.5
 @export var sprite_scene: PackedScene
 @export var collision_radius: float = 20
+@export var detection_radius: float = 2000
 
 
 func lua_fields() -> Array:
-	return ["name", "speed", "sprite_scene", "collision_radius"] + super()
+	return super() + [
+		"name", 
+		"speed", 
+		"sprite_scene", 
+		"collision_radius", 
+		"detection_radius"
+	]
