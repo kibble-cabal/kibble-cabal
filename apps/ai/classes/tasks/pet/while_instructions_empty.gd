5758fb70d48b46a53c52ea26885bd0c1a0dc724f
Audrey Serene <audrey.serene@gmail.com>
2024-01-24 16:12:15 -0600

    Modify need fulfillment behavior tree to use `BTWhileInstructionsEmpty` task (resolves #223)
    This commit also adds a check to `BTWhileInstructionsEmpty` to only abort if its not run while an instruction is already running.

--- apps/ai/classes/tasks/pet/while_instructions_empty.gd
+++ apps/ai/classes/tasks/pet/while_instructions_empty.gd
@@ -3,6 +3,8 @@ extends BTDecorator
 
 
 ## Executes the child task only if the owning pet has no instructions. If the pet does have instructions, the current task is aborted.
+##
+## [b]Note:[/b] This decorator does nothing if it is run during an instruction.
 
 ## The status to return if an instruction is found.
 @export var on_instruction_found: Status = FAILURE
@@ -19,7 +21,8 @@ func _tick(delta: float) -> Status:
 	var pet := get_pet_resource()
 	if not pet: return FAILURE
 	
-	if pet.instructions.is_empty():
+	# Execute child if there are no instructions, or if this node is playing within an instruction.
+	if pet.instructions.is_empty() or is_instruction():
 		return get_child(0).execute(delta)
 	else:
 		get_child(0).abort()
@@ -30,3 +33,7 @@ func get_pet_resource() -> PetResource:
 	if agent and "resource" in agent:
 		return agent.resource as PetResource
 	return null
+
+
+func is_instruction() -> bool:
+	return blackboard.get_var(&"context/is_instruction", false)
