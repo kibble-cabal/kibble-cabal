855d2f33d56ebb95c1920d6e910b86db3d32876c
Audrey Serene <audrey.serene@gmail.com>
2024-01-24 13:29:48 -0600

    Improve `BTNavigate` class by using LimboAI's `BBNode` and adding a max distance function

--- apps/ai/classes/task_base_classes/navigate.gd
+++ apps/ai/classes/task_base_classes/navigate.gd
@@ -5,13 +5,19 @@ class_name BTNavigate extends BTAction
 ## If the navigation stops but the target is not reached, this task fails.
 ## [b]Note:[/b] This is a base class and should probably not be used on its own.
 
-@export var navigation_agent_path: NodePath
+@export var navigation_agent_node: BBNode
 
 var navigation_agent: NavigationAgent
 
 
+func _get_configuration_warning() -> PackedStringArray:
+	var warning := PackedStringArray()
+	if not navigation_agent_node: warning.append("Missing navigation agent!")
+	return warning
+
+
 func _enter() -> void:
-	navigation_agent = agent.get_node(navigation_agent_path)
+	navigation_agent = navigation_agent_node.get_value(agent, blackboard)
 	if navigation_agent:
 		await navigation_agent.get_tree().physics_frame
 		navigation_agent.set_target_position(get_navigation_position())
@@ -25,10 +31,15 @@ func _tick(_delta: float) -> Status:
 		if navigation_agent.is_target_reached():
 			return SUCCESS
 		return FAILURE
-	#if navigation_agent.distance_to_target() < 100:
-		#return SUCCESS
+	if navigation_agent.distance_to_target() < get_max_distance():
+		navigation_agent.set_target_position(navigation_agent.character.position)
+		return SUCCESS
 	return RUNNING
 
 
 func get_navigation_position() -> Vector2:
 	return blackboard.get_var("navigation_target", Vector2.ZERO)
+
+
+func get_max_distance() -> float:
+	return 0
