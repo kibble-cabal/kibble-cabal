9ca5a8aacf4357d73f4eb15d3382911143839e1b
Audrey Serene <audrey.serene@gmail.com>
2024-01-25 12:41:32 -0600

    Deplete needs on datetime tick (resolves #233)

--- expansion_packs/core-pet/main.gd
+++ expansion_packs/core-pet/main.gd
@@ -57,3 +57,16 @@ func _init() -> void:
 	Tags.map(TagDB.register)
 	Actions.map(ActionDB.register)
 	ActionScripts.map(func(action: GDScript) -> void: ActionDB.register(action.new()))
+	
+	await Meta.systems_ready
+	DatetimeSystem.ticked.connect(deplete_needs)
+
+
+func deplete_needs() -> void:
+	for need_identifier in NeedsConfig.Needs:
+		var need := AttributeDB.find(need_identifier) as NeedAttribute
+		if not need: continue
+		for pet_node in PetSystem.pet_nodes:
+			var modifier := randf_range(0.01, 0.02) * need.depletion_rate
+			(pet_node as PetScene).ability_system.modify_attribute_value(need, -modifier)
+			
