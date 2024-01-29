71ddc51ac19466101e7b35eeeb8ba8d629a8bae7
Audrey Serene <audrey.serene@gmail.com>
2024-01-29 17:14:04 -0600

    Add new 3D Camera (resolves #255)

---
+++ apps/camera/classes/default_camera_3d.gd
@@ -0,0 +1,69 @@
+extends Camera3D
+
+enum Mode { NONE, ROTATE, PAN }
+
+@export_range(0, 10, 0.01) var sensitivity : float = 3
+@export_range(0, 1000, 0.1) var default_velocity : float = 5
+@export_range(0, 10, 0.01) var speed_scale : float = 1.17
+@export_range(1, 100, 0.1) var boost_speed_multiplier : float = 3.0
+@export var max_speed : float = 1000
+@export var min_speed : float = 0.2
+
+var mode := Mode.NONE
+
+func _input(event):
+	if not current:
+		return
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			match mode:
+				Mode.PAN: _pan(event.relative)
+				Mode.ROTATE:
+					if abs(event.relative.y) > abs(event.relative.x):
+						_zoom(event.relative.normalized().y)
+					else:
+						_rotate(event.relative * Vector2(1, 0))
+	
+	if event.is_action(&"click"):
+		mode = Mode.PAN if event.pressed else Mode.NONE
+	
+	if event.is_action(&"two_finger_click"):
+		mode = Mode.ROTATE if event.pressed else Mode.NONE
+	
+	if event.is_action(&"click") or event.is_action(&"two_finger_click"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+
+
+func _process(delta):
+	if not current:
+		return
+	
+	_pan(Input.get_vector("right", "left", "down", "up"))
+	
+	if Input.is_action_pressed("rotate_camera_left"):
+		_rotate(Vector2(-1, 0))
+	
+	if Input.is_action_pressed("rotate_camera_right"):
+		_rotate(Vector2(1, 0))
+	
+	if Input.is_action_pressed("zoom_in"):
+		_zoom(1)
+	
+	if Input.is_action_pressed("zoom_out"):
+		_zoom(-1)
+
+
+func _pan(relative: Vector2) -> void:
+	var direction := Vector3(-relative.x, relative.y, 0).normalized()
+	translate(direction / 300 * sensitivity * default_velocity)
+
+
+func _rotate(relative: Vector2) -> void:
+	rotation.y -= relative.x / 1000 * sensitivity
+	rotation.x -= relative.y / 1000 * sensitivity
+	rotation.x = clamp(rotation.x, PI / -2, PI / 2)
+
+
+func _zoom(relative: float) -> void:
+	size += relative / 30 * sensitivity * -1
