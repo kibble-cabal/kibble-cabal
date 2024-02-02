045acee2d5523fa9aa96d333fbebbd96b83ca83d
Audrey Serene <audrey.serene@gmail.com>
2024-02-01 21:18:45 -0600

    Add `Control3D` class

---
+++ apps/ui/classes/nodes/control_3d.gd
@@ -0,0 +1,16 @@
+class_name Control3D extends Control
+
+## This class inherits its position from its parent [Node3D].
+
+@export var local_position: Vector3
+
+@onready var parent := get_parent()
+@onready var viewport := get_viewport()
+@onready var camera := viewport.get_camera_3d()
+
+
+func _process(_delta: float) -> void:
+	if is_inside_tree() and visible:
+		var total_position = local_position
+		if parent is Node3D: total_position += parent.global_position
+		position = camera.unproject_position(total_position)
